31b6d06cb4849550615b4a36d98eceabce899f5d
Diego <diegondonascimento@gmail.com>
2026-05-10 19:56:10 -0300

    add config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+/* =====================================================
+   Agenda+ — config.js
+   ⚠ Este arquivo está no .gitignore — não suba para o git!
+   ===================================================== */
+
+const firebaseConfig = {
+  apiKey:            "AIzaSyBz21jxNqU6_q3spoagCboyxYHrSvRq_04",
+  authDomain:        "agenda-9ceec.firebaseapp.com",
+  projectId:         "agenda-9ceec",
+  storageBucket:     "agenda-9ceec.firebasestorage.app",
+  messagingSenderId: "401612427005",
+  appId:             "1:401612427005:web:40b5be40ba0ce261abb894",
+};
